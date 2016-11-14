319714f04b04d54a6d7c9084442768003e6d12c8
donganchao <cucdac@163.com>
2016-11-14 23:02:14 +0800

    new file:   "DataStructures/\347\272\277\346\200\247\350\241\250/Delete_LinkList.cpp" 	new file:   "DataStructures/\347\272\277\346\200\247\350\241\250/Insert_LinkList.cpp"
    添加链表插入和按值删除算法

---
+++ DataStructures/线性表/Delete_LinkList.cpp
@@ -0,0 +1,22 @@
+﻿#include "LinkList.h"
+//删除链表中第一个值为x的结点
+
+LinkList* deleteLinkList(LinkList *L,int x){     //*L为头结点，假设元素x为int型
+	LinkList *p,*q;
+	p = L;
+	while (p->next != NULL && p->next->data != x){
+		p = p->next;
+	}
+	if(p->next->data == x){
+		q = p->next;
+		p->next = q->next;
+		delete q;
+		return L;
+	}
+	else{
+		printf("删除的元素不存在，删除失败！");
+		return L;
+	}
+}
+
+		
---
+++ DataStructures/线性表/Insert_LinkList.cpp
@@ -0,0 +1,33 @@
+﻿#include "LinkList.h"
+//在第i个结点后插入元素x（第一个结点为头结点，假设x为int型）
+
+
+LinkList* insertLinkList(LinkList *L,int i,int x){   //*L为头结点
+	if(i<1){
+		printf("插入失败，请输入大于1的整数！");
+		return L;
+	}
+
+	int n=1;  //n用来统计当前链表结点数
+	LinkList *p = L; //初始从头结点开始统计
+	while (p->next != NULL){
+		p = p->next;
+		n++;
+	}
+
+	if(i>n){
+		printf("插入失败，插入位置超出链表长度！");
+		return L;
+	}
+
+	LinkList *r,*s;		//*r是第i个结点，*s是插在后面的新结点
+	r = L;
+	for(int j=1;j<i;j++){
+		r = r->next;
+	}
+	s = new LinkList;
+	s->data = x;
+	s->next = r->next;
+	r->next = s;
+	return L;
+}
