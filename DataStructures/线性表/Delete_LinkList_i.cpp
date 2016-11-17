b9cb8caa476c9bf45d2d64ab53fcfe22f378d176
donganchao <cucdac@163.com>
2016-11-17 21:14:00 +0800

    deleted:    "DataStructures/\347\272\277\346\200\247\350\241\250/Delete_LinkList.cpp" 	new file:   "DataStructures/\347\272\277\346\200\247\350\241\250/Delete_LinkList_i.cpp" 	new file:   "DataStructures/\347\272\277\346\200\247\350\241\250/Delete_LinkList_x.cpp"
    加入按位置删除结点算法

---
+++ DataStructures/线性表/Delete_LinkList_i.cpp
@@ -0,0 +1,28 @@
+﻿#include "LinkList.h"
+//删除第i个结点，头结点除外（按位置）
+
+LinkList* deleteLinkList_i(LinkList *L,int i){
+	LinkList *p,*r,*s;
+	
+	int n=1;  //n用来统计当前链表结点数，头结点为第一个结点
+	p = L; //初始从头结点开始统计
+	while (p->next != NULL){
+		p = p->next;
+		n++;
+	}
+
+	if(i<=1||i>n){
+		printf("删除的位置不合理，删除失败！");
+		return L;
+	}
+	else{
+		r = L;
+		int j = 1;
+		while (j < i-1){    //找到要删除结点的前一个结点
+			r = r->next;
+		}
+		s = r->next;
+		r->next = s->next;
+		return L;
+	}
+}
--- DataStructures/线性表/Delete_LinkList_x.cpp
+++ DataStructures/线性表/Delete_LinkList_x.cpp
@@ -1,10 +1,10 @@
 ﻿#include "LinkList.h"
-//删除链表中第一个值为x的结点
+//删除链表中第一个值为x的结点(按值)
 
-LinkList* deleteLinkList(LinkList *L,int x){     //*L为头结点，假设元素x为int型
+LinkList* deleteLinkList_x(LinkList *L,int x){     //*L为头结点，假设元素x为int型
 	LinkList *p,*q;
 	p = L;
-	while (p->next != NULL && p->next->data != x){
+	while (p->next != NULL && p->next->data != x){   //找要删除结点的前一个结点
 		p = p->next;
 	}
 	if(p->next->data == x){
