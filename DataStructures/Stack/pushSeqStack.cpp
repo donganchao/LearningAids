26addf39a59b428cebda50efb6a18566b1240442
donganchao <cucdac@163.com>
2016-12-06 22:32:48 +0800

    new file:   DataStructures/Stack/pushSeqStack.cpp

---
+++ DataStructures/Stack/pushSeqStack.cpp
@@ -0,0 +1,14 @@
+﻿#include "SeqStack.h"
+
+bool pushSeqStack(SeqStack &S,int x){		//假设入栈元素为int类型
+	if(S.length == MAXSIZE){
+		printf("入栈失败，栈已满！");
+		return false;
+	}
+	else{
+		*S.top = x;
+		S.top ++;
+		S.length ++;
+		return true;
+	}
+}
